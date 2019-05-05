be8eac9b09684c67cb778a835cfa6b70186c368a
azxca1731 <azxca1731@gmail.com>
2019-05-05 10:42:07 +0900

    remove elastic search from connection

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,21 +1,5 @@
 const Query = {
-	async connection(parent, args, { reques, elastic }, info) {
-		try {
-			const result = await elastic.search({
-				index: "basketball",
-				body: {
-					query: {
-						term: {
-							points: 30
-						}
-					}
-				}
-			});
-			console.log(result.body.hits.hits[0]._source);
-		} catch (err) {
-			throw new Error(err);
-		}
-
+	async connection(parent, args, context, info) {
 		return true;
 	},
 	async search(
