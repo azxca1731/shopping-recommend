6e16306e3f633a67f70873cb0e7f38aa963e53cf
azxca1731 <azxca1731@gmail.com>
2019-05-06 16:37:06 +0900

    add TODO

--- src/Apollo.js
+++ src/Apollo.js
@@ -31,12 +31,13 @@ const Client = new ApolloClient({
 	resolvers: {
 		Query: {
 			getSearchedArray: () => [
+				/* TODO: 여기에 예전 대화 목록 요청 issue_5
 				{
 					message: "제발 되라",
 					me: true,
 					id: uuid(),
 					__typename: "Message"
-				}
+				}*/
 			]
 		},
 		Mutation: {
@@ -54,6 +55,8 @@ const Client = new ApolloClient({
 				const data = {
 					getSearchedArray: [...previousArray, newMessage]
 				};
+				/* TODO1: 서버에 데이터 요청 검색 쿼리 요청후 그 검색 데이터를 캐쉬에 쓰기 issue_6 */
+				/* TODO2: newMessage를 예전 대화를 가져올 때 사용하기 위해 서버에 전송 issue_5 */
 				cache.writeQuery({ query, data });
 				return newMessage;
 			}
