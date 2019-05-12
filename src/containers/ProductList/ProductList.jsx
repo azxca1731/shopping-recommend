c61eeee2b70713a5f4caec307b02e2bd01112089
azxca1731 <azxca1731@gmail.com>
2019-05-13 01:21:06 +0900

    add query for search

--- src/containers/ProductList/ProductList.jsx
+++ src/containers/ProductList/ProductList.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import { Query } from "react-apollo";
+
+import { SEARCH } from "queries";
 
 const ProductListDiv = styled.div``;
 
+const ProductRow = styled.div`
+	border: 1px solid #000;
+`;
+
 const ProductList = ({ query }) => {
-	return <ProductListDiv>{query}</ProductListDiv>;
+	const [currentFrom, setFrom] = useState(0);
+	return (
+		<ProductListDiv>
+			<Query
+				query={SEARCH}
+				variables={{ query, from: currentFrom, size: 5 }}
+			>
+				{({ loading, error, data: { search } }) => {
+					if (loading) return "loading";
+					if (error) return `Error! ${error}`;
+					return search
+						? search.map(({ name, id }, index) => (
+								<ProductRow key={id}>
+									{currentFrom + index + 1} {name}
+								</ProductRow>
+						  ))
+						: null;
+				}}
+			</Query>
+			<button onClick={() => setFrom(currentFrom - 5)}>이전</button>
+			<button onClick={() => setFrom(currentFrom + 5)}>다음</button>
+		</ProductListDiv>
+	);
 };
 
 ProductList.propTypes = {
