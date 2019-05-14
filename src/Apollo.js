38a4b2cab61d1421283d66bcb34d7925cb1d7623
azxca1731 <azxca1731@gmail.com>
2019-05-14 14:48:00 +0900

    add setting for react websocket link

--- src/Apollo.js
+++ src/Apollo.js
@@ -1,12 +1,33 @@
-import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
+import { ApolloClient, HttpLink, split } from "apollo-boost";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 import gql from "graphql-tag";
 import uuid from "uuid/v1";
 
 import { READ_SEARCHED_ARRAY, GET_SEARCHED_ARRAY } from "queries";
 
 const cache = new InMemoryCache();
+const wsLink = new WebSocketLink({
+	uri: `ws://localhost:4000`,
+	options: {
+		reconnect: true
+	}
+});
+
+const httpLink = new HttpLink({
+	uri: "http://localhost:4000/",
+	credentials: "include"
+});
+
+const link = split(
+	({ query }) => {
+		const { kind, operation } = getMainDefinition(query);
+		return kind === "OperationDefinition" && operation === "subscription";
+	},
+	wsLink,
+	httpLink
+);
 
 const typeDefs = gql`
 	extend type Query {
@@ -27,10 +48,7 @@ const typeDefs = gql`
 
 const Client = new ApolloClient({
 	cache,
-	link: new HttpLink({
-		uri: "http://localhost:4000/",
-		credentials: "include"
-	}),
+	link,
 	typeDefs,
 	resolvers: {
 		Query: {
