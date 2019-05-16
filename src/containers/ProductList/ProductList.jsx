36d8e794d9dea8bcbad2c8ef676a62b4784f8bc7
azxca1731 <azxca1731@gmail.com>
2019-05-16 23:18:56 +0900

    카테고리 아이디가 있을시 카테고리 검색으로 변경

--- src/containers/ProductList/ProductList.jsx
+++ src/containers/ProductList/ProductList.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import { Query } from "react-apollo";
 
-import { SEARCH } from "queries";
+import { SEARCH, SEARCH_BY_CATEGORY } from "queries";
 
 const ProductListDiv = styled.div``;
 
@@ -11,26 +11,43 @@ const ProductRow = styled.div`
 	border: 1px solid #000;
 `;
 
-const ProductList = ({ query }) => {
+const ProductList = ({ query, categoryId }) => {
 	const [currentFrom, setFrom] = useState(0);
 	let total;
 	return (
 		<ProductListDiv>
 			<Query
-				query={SEARCH}
-				variables={{ query, from: currentFrom, size: 5 }}
+				query={categoryId ? SEARCH_BY_CATEGORY : SEARCH}
+				variables={
+					categoryId
+						? { categoryId, from: currentFrom, size: 5 }
+						: { query, from: currentFrom, size: 5 }
+				}
 			>
-				{({ loading, error, data: { search } }) => {
+				{({ loading, error, data: { search, searchByCategory } }) => {
 					if (loading) return "loading";
 					if (error) return `Error! ${error}`;
-					total = search.total;
-					return search
-						? search.productList.map(({ name, id }, index) => (
-								<ProductRow key={id}>
-									{currentFrom + index + 1} {name}
-								</ProductRow>
-						  ))
-						: null;
+					if (categoryId) {
+						total = searchByCategory.total;
+						return searchByCategory
+							? searchByCategory.productList.map(
+									({ name, id }, index) => (
+										<ProductRow key={id}>
+											{currentFrom + index + 1} {name}
+										</ProductRow>
+									)
+							  )
+							: null;
+					} else {
+						total = search.total;
+						return search
+							? search.productList.map(({ name, id }, index) => (
+									<ProductRow key={id}>
+										{currentFrom + index + 1} {name}
+									</ProductRow>
+							  ))
+							: null;
+					}
 				}}
 			</Query>
 			<button
@@ -50,7 +67,8 @@ const ProductList = ({ query }) => {
 };
 
 ProductList.propTypes = {
-	query: PropTypes.string.isRequired
+	query: PropTypes.string,
+	categoryId: PropTypes.number
 };
 
 export default ProductList;
