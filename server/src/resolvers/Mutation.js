b74f51cb559d3c0a6851e4d48de1a0de470d42a8
azxca1731 <azxca1731@gmail.com>
2019-05-16 23:17:28 +0900

    추천이라는 쿼리어면 카테고리 메시지로 저장  및 가장 많이 검색된 카테고리 저장

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -4,10 +4,9 @@ const Mutation = {
 		{ query },
 		{
 			mongo: { Message },
-			request: {
-				session: { user }
-			},
-			pubsub
+			request: { session },
+			pubsub,
+			elastic
 		},
 		info
 	) {
@@ -16,24 +15,91 @@ const Mutation = {
 				message: query,
 				me: true,
 				query: "",
-				owner: user,
+				owner: session.user,
 				created_date: Date.now() + 9 * 1000 * 60 * 60
 			});
 			newMessage.save();
-			pubsub.publish(`MESSAGE-${user._id}`, {
+			pubsub.publish(`MESSAGE-${session.user._id}`, {
 				Message: { ...newMessage._doc, id: newMessage._id }
 			});
-			const serverMessage = new Message({
-				message: "",
-				query,
-				me: false,
-				owner: user,
-				created_date: Date.now() + 9 * 1000 * 60 * 60 + 50
-			});
-			serverMessage.save();
-			pubsub.publish(`MESSAGE-${user._id}`, {
-				Message: { ...serverMessage._doc, id: serverMessage._id }
-			});
+			if (query === "추천") {
+				if (!session.categoryMost) {
+					throw new Error("현재 검색된 데이터가 없습니다.");
+				}
+				const [[first]] = Object.entries(session.categoryMost).sort(
+					([_, firstNum], [_1, secondNum]) => secondNum - firstNum
+				);
+				const serverMessage = new Message({
+					message: "",
+					query,
+					categoryId: first,
+					me: false,
+					owner: session.user,
+					created_date: Date.now() + 9 * 1000 * 60 * 60 + 50
+				});
+				serverMessage.save();
+				pubsub.publish(`MESSAGE-${session.user._id}`, {
+					Message: { ...serverMessage._doc, id: serverMessage._id }
+				});
+			} else {
+				const serverMessage = new Message({
+					message: "",
+					query,
+					me: false,
+					owner: session.user,
+					created_date: Date.now() + 9 * 1000 * 60 * 60 + 50
+				});
+				serverMessage.save();
+				pubsub.publish(`MESSAGE-${session.user._id}`, {
+					Message: { ...serverMessage._doc, id: serverMessage._id }
+				});
+
+				//추천을 위한 카테고리화
+				const {
+					body: {
+						hits: { hits }
+					}
+				} = await elastic.search({
+					index: "shopping",
+					body: {
+						from: 0,
+						size: 10,
+						query: {
+							function_score: {
+								query: {
+									match: { name: query }
+								},
+								script_score: {
+									script: {
+										lang: "expression",
+										source:
+											"(doc['popularity'].value +1) * _score"
+									}
+								}
+							}
+						}
+					}
+				});
+				let usermost = {};
+				hits.map(({ _source: { categoryId } }) => {
+					if (usermost[`${categoryId}`]) usermost[`${categoryId}`]++;
+					else usermost[`${categoryId}`] = 1;
+					return false;
+				});
+				const [[first]] = Object.entries(usermost).sort(
+					([_, firstNum], [_1, secondNum]) => secondNum - firstNum
+				);
+				if (!session.categoryMost) {
+					session.categoryMost = {};
+					session.categoryMost[`${first}`] = 3;
+				} else {
+					if (session.categoryMost[`${first}`])
+						session.categoryMost[`${first}`] += 3;
+					else session.categoryMost[`${first}`] = 3;
+				}
+				session.save();
+			}
+
 			return true;
 		} catch (err) {
 			throw new Error(err);
